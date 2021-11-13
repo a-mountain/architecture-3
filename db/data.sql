ad01ef18c6e30e334d6886067e53574256566f7a
a-mountain <frostnext2@gmail.com>
2021-11-13 14:03:29 +0200

    Add database schema and example data

---
+++ db/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO MENU_ITEMS(NAME, PRICE)
+VALUES ('Beef carpaccio', 790),
+       ('Burrata', 950),
+       ('salmon', 870),
+       ('Herring with new potatoes and green onions', 2100),
+       ('Antipasto', 550),
+       ('Oyster', 390),
+       ('Spicy beef tartare with truffle oil', 790);
+
+INSERT INTO TAXES(TAX_FACTOR)
+VALUES (0.3);
+
+INSERT INTO TIPS(TIP_FACTOR)
+VALUES (0.1);
